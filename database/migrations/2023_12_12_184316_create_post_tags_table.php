889077b76aefd89f766828112380e760c5447e63
Yakov Lemenkov <yakoff4@gmail.com>
2023-12-14 22:38:32 +0300

    add factories

--- database/migrations/2023_12_12_184316_create_post_tags_table.php
+++ database/migrations/2023_12_12_184316_create_post_tags_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->index('post_id', 'post_tag_post_idx');
             $table->index('tag_id', 'post_tag_tag_idx');
             
-            $table->foreign('post_id', 'post_tag_post_fk')->on('posts')->references('id');
+            // $table->foreign('post_id', 'post_tag_post_fk')->on('posts')->references('id');
             $table->foreign('tag_id', 'post_tag_tag_fk')->on('tags')->references('id');
             
             $table->timestamps();
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Tag;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +22,14 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Category::factory(20)->create();
+        $tags = Tag::factory(50)->create();
+        $posts = Post::factory(200)->create();
+
+        foreach($posts as $post) {
+            $tagsIds = $tags->random(5)->pluck('id');
+            $post->tags()->attach($tagsIds);
+        }
     }
 }
